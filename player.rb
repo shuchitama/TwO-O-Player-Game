d881059bfdd4a29dd1ccbdfaeabe0d04ca1cd0f5
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-30 16:09:06 -0700

    implement player class

--- player.rb
+++ player.rb
@@ -1,2 +1,13 @@
-class player
+class Player
+  attr_accessor :lives, :score
+  def initialize(name)
+    @name = name
+    @lives = 3
+    @score = 0
+  end
 end
+
+p1 = Player.new("Player1")
+p1.score = p1.score + 1
+p1.lives = p1.lives - 1
+puts p1.inspect
