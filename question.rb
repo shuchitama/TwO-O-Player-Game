c935a13daf4c8ab900438f5b3a6783a4f73489bc
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-30 16:17:18 -0700

    implement question class

--- question.rb
+++ question.rb
@@ -1,2 +1,16 @@
-class question
+class Question
+  RAND_NUM_1 = rand(20)
+  RAND_NUM_2 = rand(20)
+  
+  def ask
+    @qn = "What does #{RAND_NUM_1} + #{RAND_NUM_2} equal?"
+    @ans = RAND_NUM_1 + RAND_NUM_2
+    return {"question" => @qn, "answer" => @ans}
+  end
+
 end
+
+q1 = Question.new
+ret = q1.ask
+puts ret["question"].inspect
+puts ret["answer"].inspect
