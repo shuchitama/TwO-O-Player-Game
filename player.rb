0fa10aad944317cfa20720305d3b2ab97aa35180
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-30 18:49:32 -0700

    add gameover method

--- player.rb
+++ player.rb
@@ -1,12 +1,11 @@
 class Player
-  attr_accessor :lives
+  attr_accessor :lives, :name
   def initialize(name)
     @name = name
     @lives = 3
   end
-end
 
-# p1 = Player.new("Player1")
-# p1.score = p1.score + 1
-# p1.lives = p1.lives - 1
-# puts p1.inspect
+  def gameover
+    self.lives == 0 ? true : false
+  end
+end
