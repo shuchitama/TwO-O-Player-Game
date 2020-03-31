49425841b4ed7022d5a901f04e592043e71cb130
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-30 18:50:06 -0700

    change ask method, add check_answer method

--- question.rb
+++ question.rb
@@ -1,21 +1,17 @@
 class Question
-  RAND_NUM_1 = rand(20)
-  RAND_NUM_2 = rand(20)
-  
+  attr_reader :RAND_NUM_1, :RAND_NUM_2
+
+  def initialize
+    @RAND_NUM_1 = rand(20)
+    @RAND_NUM_2 = rand(20)
+  end
+
   def ask
-    puts "What does #{RAND_NUM_1} + #{RAND_NUM_2} equal?"
+    "What does #{@RAND_NUM_1} + #{@RAND_NUM_2} equal?"
   end
 
   def check_answer(ans)
-    if RAND_NUM_1 + RAND_NUM_2 == ans
-      true
-    end
-      false
+    @RAND_NUM_1 + @RAND_NUM_2 == ans ? true : false
   end
 
 end
-
-# q1 = Question.new
-# ret = q1.ask
-# puts ret["question"].inspect
-# puts ret["answer"].inspect
